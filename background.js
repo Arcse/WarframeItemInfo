9401c331e8a1993babb65deee6eed0615a3263b5
Arcs <Kevinlie10@gmail.com>
2019-01-03 01:53:37 -0500

    Implemented jQuery and Regex for course code

--- background.js
+++ background.js
@@ -4,16 +4,19 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     if (buttonState === "on") {
         localStorage.buttonState = "off";
         buttonState = localStorage.buttonState;
+        alert("extension is off");
     } 
 
     else if (buttonState === "off") {
         localStorage.buttonState = "on";
         buttonState = localStorage.buttonState;
+        alert("extension is on");
     }
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status === 'complete' && buttonState === "on") {
+    if (changeInfo.status === 'complete' && buttonState === "on" && /.*google\....?\/search\?.*/.test(tab.url)) {
+        alert("updated");
         chrome.tabs.executeScript(tab.id, {file: 'createGoogleCard.js'});
     }
 });
--- createGoogleCard.js
+++ createGoogleCard.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
-    let url = location.search.substr(1).split("&");
-    
-
-    if(/[a-zA-Z]{2,}\+?[0-9]{3}[a-zA-Z]?/.test(queryDict["q"])){
-        getInfo(queryDict["q"].replace("+", ""));
+    let query = location.search.substr(1).split("&");
+    let courseCode = query[1].slice(query[1].indexOf("=") + 1);
+    alert(courseCode);
+    if(/[a-zA-Z]{2,}\+?[0-9]{3}[a-zA-Z]?/.test(courseCode)){
+        getInfo(courseCode);
     }
 });
+
+function getInfo(courseCode){
+    document.body.style.background = 'green'
+}
