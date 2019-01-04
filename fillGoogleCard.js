5d3b5adec6cc3847530f38a813eb62b14c20bb24
Arcs <Kevinlie10@gmail.com>
2019-01-03 21:37:57 -0500

    Implemented getCourseNumber and getCourseName

--- fillGoogleCard.js
+++ fillGoogleCard.js
@@ -1,16 +1,29 @@
 $(document).ready(function () {
-    getCourseNumber(courseCode);
-    getCourseName(courseCode);
-    getCourseInfo(courseCode);
+    let courseName = getCourseName(courseCode);
+    let courseNumber = getCourseNumber(courseCode);
+    // getCourseInfo(courseName, courseNumber);
 });
 
-function getCourseNumber(courseCode){
-
-}
-
 function getCourseName(courseCode){
-
+    let courseName = "";
+    for (let i = 0; i < courseCode.length; i++) {
+        if (/[a-zA-Z]/.test(courseCode.charAt(i))){
+            courseName += courseCode.charAt(i);
+        }
+        else {
+            break;
+        }
+    }
+    return courseName;
 }
-function getCourseInfo(courseCode){
 
+function getCourseNumber(courseCode){
+    for (let i = 0; i < courseCode.length; i++) {
+        if (/[0-9]/.test(courseCode.charAt(i))){
+            return courseCode.substring(i);
+        }   
+    }
 }
+
+// function getCourseInfo(courseName, courseNumber){
+// }
