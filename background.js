b0dcc2fbde81e5337eec0aa1e84740739d92e34f
Arcs <Kevinlie10@gmail.com>
2019-01-03 21:23:17 -0500

    Major refactor on  chrome.tabs.onUpdated

--- background.js
+++ background.js
@@ -15,8 +15,18 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status === 'complete' && buttonState === "on" && /.*google\....?\/search\?.*/.test(tab.url)) {
-        alert("updated");
-        chrome.tabs.executeScript(tab.id, {file: 'createGoogleCard.js'});
+    if (changeInfo.status === 'complete' && buttonState === "on" && /.*google\....?\/search\?.*/.test(tab.url)){
+        var query = tab.url.substr(1).split("&");
+        var userInput = query[1].slice(query[1].indexOf("=") + 1);
+        if (/[a-zA-Z]{2,}\+?[0-9]{3}[a-zA-Z]?/.test(userInput)) {
+            chrome.tabs.executeScript(tab.id, 
+                // Setting paremeter before execution of JS file
+                {code: 'var courseCode = ' + JSON.stringify(userInput)}, 
+                function() {
+                    // Executing the JS file.
+                    chrome.tabs.executeScript(tab.id, {file: "createGoogleCard.js"});
+                    chrome.tabs.executeScript(tab.id, {file: "fillGoogleCard.js"});
+            });
+        }
     }
 });
---
+++ fillGoogleCard.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    getCourseNumber(courseCode);
+    getCourseName(courseCode);
+    getCourseInfo(courseCode);
+});
+
+function getCourseNumber(courseCode){
+
+}
+
+function getCourseName(courseCode){
+
+}
+function getCourseInfo(courseCode){
+
+}
