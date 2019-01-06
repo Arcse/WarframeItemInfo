dee176aff7b45f525b560248a5d3660d6c6cd1f6
Arcs <Kevinlie10@gmail.com>
2019-01-06 17:47:04 -0500

    minor improvements

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,4 @@
-let buttonState = (chrome.storage.local.get(['buttonState'], function(result) {})) || "on"
+let buttonState = (chrome.storage.sync.get(['buttonState'], function(result) {})) || "on"
 
 var on = {
     type: "basic",
@@ -21,14 +21,14 @@ var off = {
 
 chrome.browserAction.onClicked.addListener(function (tab) {
     if (buttonState === "on") {
-        chrome.storage.local.set({'buttonState': "off"}, function() {});
+        chrome.storage.sync.set({'buttonState': "off"}, function() {});
         buttonState = "off";
         chrome.notifications.create(off);
         // chrome.browserAction.setIcon({path:"images/UWWhite128x.png"});
     } 
 
     else if (buttonState === "off") {
-        chrome.storage.local.set({'buttonState': "on"}, function() {});
+        chrome.storage.sync.set({'buttonState': "on"}, function() {});
         buttonState = "on";
         chrome.notifications.create(on);
         // chrome.browserAction.setIcon({path:"images/UWYellow128x.png"});
